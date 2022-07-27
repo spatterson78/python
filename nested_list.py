50413dde6a3e4762a013337539f91ceea07f1ea0
Shawn Patterson <spatterson@rhel8-buildsrv.example.com>
2022-07-27 16:58:10 -0700

    Adding system_check.py and nested_list.py scripts

---
+++ nested_list.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+#
+#######################################################
+#
+# Filename: nested_list.py
+# Author: Shawn Patterson
+# Description: Script to iterate through nested list
+# Usage: ./nested_list.py
+#
+#######################################################
+
+# Import modules
+import subprocess
+import getpass
+
+# Define nested list
+nlist = [["Current working directory:", "pwd"],
+         ["Current working directory contents:", "ls -lahtr"],
+         ["Current filesystem usage:", "df -h"]]
+
+# Print current user
+print()
+print("Current user is: " + getpass.getuser())
+print()
+
+# Loop through list to provide output
+for text, cmd in nlist:
+    proc_cmd = subprocess.Popen(
+        [cmd],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+        shell=True
+    )
+
+    cmd_output, cmd_error = proc_cmd.communicate()
+
+    print(text + '\n' + cmd_output + cmd_error)
---
+++ system_check.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+#
+####################################################################
+#
+# Filename: system_check.py
+# Author: Shawn Patterson
+# Description: System agnostic script to get basic system info
+# Usage: ./system_check.py (Linux) or py system_check.py (Windows)
+#
+####################################################################
+
+# Import modules
+import platform
+import getpass
+import psutil  # Important, might need to install psutil module on target systems
+
+
+# Variables
+get_os = platform.system()
+ram_pct = psutil.virtual_memory()[2]
+
+# Show current user & output header
+print()
+print("Hello " + getpass.getuser(), ", current system information:")
+print("----------------------------------------------")
+print()
+
+# Get system platform (Windows, Linux, UNIX, etc,)
+if get_os == "Windows":
+    print("System Platform:", platform.system(), platform.version(), platform.machine())
+if get_os == "Linux":
+    print("System Platform:", platform.system(), platform.release(), platform.machine())
+print()
+
+# Get system hostname/computer name
+print("System hostname:", platform.node())
+print()
+
+# Get CPU usage information
+print("Gathering system CPU usage over 5 second interval, please wait...")
+print()
+cpu_pct = psutil.cpu_percent(5)
+if cpu_pct > 90:
+    print("System CPU usage is above 90% threshold:", str(cpu_pct) + "%")
+if cpu_pct < 90:
+    print("System CPU usage is below 90% threshold:", str(cpu_pct) + "%")
+print()
+
+# Get RAM usage information
+if ram_pct > 85:
+    print("System RAM usage is above 85% threshold:", str(ram_pct) + "%")
+if ram_pct < 85:
+    print("System RAM usage is below 85% threshold:", str(ram_pct) + "%")
+print()
+
+# Get filesystem information
+if get_os == "Windows":
+    windows_disk_pct = psutil.disk_usage("C:/")[3]
+    if windows_disk_pct > 85:
+        print("System filesystem usage for C is above 90% threshold:", str(windows_disk_pct) + "%")
+    if windows_disk_pct < 85:
+        print("System filesystem usage for C is below 90% threshold:", str(windows_disk_pct) + "%")
+if get_os == "Linux":
+    linux_disk_pct = psutil.disk_usage("/")[3]
+    if linux_disk_pct > 85:
+        print("System filesystem usage for / is above 85% threshold:", str(linux_disk_pct) + "%")
+    if linux_disk_pct < 85:
+        print("System filesystem usage for / is below 85% threshold:", str(linux_disk_pct) + "%")
+print()
+
+# Get list of running processes
+pid_list = list()
+for proc in psutil.process_iter():
+    pid_dict = proc.as_dict(attrs=['pid', 'name', 'cpu_percent', 'memory_percent'])
+    pid_list.append(pid_dict)
+print("System processes:"'\n')
+for pid in pid_list:
+    print(pid)
